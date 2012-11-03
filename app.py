e1fec051cd9c8d34c125055b1ec57c19f2958d51
Philip Hayes <philiphayes9@gmail.com>
2012-11-03 00:40:52 -0700

    foo

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def show_random():
 class RandomValues:
     def __init__(self):
         # Get the theme to be used
-        self.theme = getTheme
+        self.theme = self.getTheme()
 
         # Should our logo be on the left?
         self.logoLeft = bool(random.getrandbits(1))
 
         # Get the buttons to be in the left and right of the navbar
         buttons = ['About', 'Register', 'Support', 'Blog', 'Contact Us', 'Sign in']
-        i = random.randint(0,len(buttons))
+        i = random.randint(1,len(buttons))
         j = random.randint(i,len(buttons))
         k = random.randint(j,len(buttons))
         self.leftButtons = buttons[:i]
