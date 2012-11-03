cd06427c16d3e8b37ed99c787058dad1674081fe
Philip Hayes <philiphayes9@gmail.com>
2012-11-03 05:48:00 -0700

    3 feature block

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+from os import listdir
+from os.path import isdir
 import random
 app = Flask(__name__)
 
@@ -15,31 +17,45 @@ class RandomValues:
         # Get the theme to be used
         self.theme = self.getTheme()
 
-        self.navTop = randBool()
-
         # Get the buttons to be in the left and right of the navbar
         buttons = ['Documentation', 'Press', 'API', 'Developers', 'Pricing', 'About', 'Register', 'Support', 'Blog', 'Contact Us', 'Sign in']
         numButtons = random.randint(0,6)
 
-        if self.navTop:
-            buttonsChosen = random.sample(buttons, numButtons)
+        buttonsChosen = random.sample(buttons, numButtons)
 
-            i = random.randint(0,numButtons)
-            j = random.randint(i,numButtons)
-            self.leftButtons = [self.name]
-            self.leftButtons.extend(list(buttonsChosen)[:i])
-            self.rightButtons = list(buttonsChosen)[i:j]
-        else:
-            self.buttons = list(random.sample(buttons, numButtons))
+        i = random.randint(0,numButtons)
+        j = random.randint(i,numButtons)
+        self.leftButtons = [self.name]
+        self.leftButtons.extend(list(buttonsChosen)[:i])
+        self.rightButtons = list(buttonsChosen)[i:j]
 
         self.searchBar = randBool()
 
         bottomButtons = ['Contact', 'Copyright', 'Privacy', 'Roadmap', 'Sitemap', 'Changes', 'Jobs', 'Status']
         self.bottomButtons = random.sample(bottomButtons, random.randint(1,len(bottomButtons)))
 
+        self.feature3 = True
+        if self.feature3:# randBool(): # build 3 feature
+            self.generate3feature()
+
+    def generate3feature(self):
+        base_path = "static/img/feature3"
+        img_group = random.choice(listdir("static/img/feature3"))
+        imgs = map(lambda x: "static/img/feature3/" + img_group + "/" + x, listdir(base_path + "/" + img_group))
+        random.shuffle(imgs)
+        self.feature3s = []
+        for i in range(3):
+            self.feature3s.append(Feature(imgs[i], "tagline", "some text"))
+
     def getTheme(self):
         return 'theme' + str(random.randint(1,11)) + '.css'
 
+
+class Feature():
+    def __init__(self, img, tagline, text):
+        self.img = img
+        self.tagline = tagline
+        self.text = text
 
 def randBool():
     return bool(random.getrandbits(1))
